f075d35aae0541772925dd23660014c1d18aa1ea
Prince Raj <games.princeraj@gmail.com>
2021-07-08 16:14:58 +0530

    static added

--- goat.py
+++ goat.py
@@ -1,12 +1,17 @@
-# with open("goat.txt",'w') as gt:
-#     for i in range(1,13):
-#         gt.write(f"'D:/paker/exteriors/houses_exterior_{str(i).zfill(3)}.jpg',"
-#               f" 'D:/paker/interiors/houses_interior_{str((6*(i-1)+1)%30).zfill(3)}.jpg',"
-#               f"'D:/paker/interiors/houses_interior_{str((6*(i-1)+2)%30).zfill(3)}.jpg',"
-#               f" 'D:/paker/interiors/houses_interior_{str((6*(i-1)+3)%30).zfill(3)}.jpg',"
-#               f"'D:/paker/interiors/houses_interior_{str((6*(i-1)+4)%30).zfill(3)}.jpg',"
-#               f" 'D:/paker/interiors/houses_interior_{str((6*(i-1)+5)%30).zfill(3)}.jpg',"
-#               f"'D:/paker/interiors/houses_interior_{str((6*(i-1)+6)%30).zfill(3)}.jpg', true, '2019-{str(i).zfill(2)}-08 04:05:06', {i%10+1})\n")
+# # with open("goat.txt",'w') as gt:
+# #     for i in range(1,13):
+# #         gt.write(f"'D:/paker/exteriors/houses_exterior_{str(i).zfill(3)}.jpg',"
+# #               f" 'D:/paker/interiors/houses_interior_{str((6*(i-1)+1)%30).zfill(3)}.jpg',"
+# #               f"'D:/paker/interiors/houses_interior_{str((6*(i-1)+2)%30).zfill(3)}.jpg',"
+# #               f" 'D:/paker/interiors/houses_interior_{str((6*(i-1)+3)%30).zfill(3)}.jpg',"
+# #               f"'D:/paker/interiors/houses_interior_{str((6*(i-1)+4)%30).zfill(3)}.jpg',"
+# #               f" 'D:/paker/interiors/houses_interior_{str((6*(i-1)+5)%30).zfill(3)}.jpg',"
+# #               f"'D:/paker/interiors/houses_interior_{str((6*(i-1)+6)%30).zfill(3)}.jpg', true, '2019-{str(i).zfill(2)}-08 04:05:06', {i%10+1})\n")
+#
+#
+# title, address, city, state, zipcode, description, price, bedrooms, bathrooms, garage, sqft, lot_size, photo_main, photo_1, photo_2, photo_3, photo_4, photo_5, photo_6, is_published, list_date, realtor_id
 
 
-title, address, city, state, zipcode, description, price, bedrooms, bathrooms, garage, sqft, lot_size, photo_main, photo_1, photo_2, photo_3, photo_4, photo_5, photo_6, is_published, list_date, realtor_id
+# psql --host=ec2-52-45-183-77.compute-1.amazonaws.com --port=5432 --username=tjlooptivvqqfk --password --dbname=d4sa61bhknctoi
+
+#pass = 8091aab12543fde5f9a0b2ca712c43d500325ef504db6fac3ed7fd3e15d20954
